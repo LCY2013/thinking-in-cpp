d6ad4baa85ec2bac6127a253288e6418a3b669e6
扶风 <fufeng@staruniongame.com>
2023-12-06 17:22:33 +0800

    Feat: c++ code with g++

--- basic/RAII/HeapObjectWrapper.cpp
+++ basic/RAII/HeapObjectWrapper.cpp
@@ -3,6 +3,12 @@
 //
 #include <iostream>
 
+// 指定c++语言规范：g++ -g -o heap HeapObjectWrapper.cpp -std=c++11
+// Makefile文件编译项目时可以这样指定：make CXXFLAGS="-g -O0 -std=c++11"
+// 使用cmake编译项目时可以在CMakeLists.txt文件中添加如下行：
+// set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -g -Wall -O0 -Wno-unused-variable")
+// -std 选项的值可以按需设置成 c++14 或 c++17 或 c++20。
+
 class HeapObjectWrapper {
 private:
     char *m_p;
