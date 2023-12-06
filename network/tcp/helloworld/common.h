c4fb3534a2854709b6d32c7b2bb26c520c71fe79
扶风 <fufeng@staruniongame.com>
2023-12-06 15:23:53 +0800

    Feat: hello world

---
+++ network/tcp/helloworld/common.h
@@ -0,0 +1,28 @@
+//
+// Created by fufeng on 2023/12/6.
+//
+
+#ifndef THINKING_IN_CPP_COMMON_H
+#define THINKING_IN_CPP_COMMON_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> /* superset of previous */
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define HW_PORT 6000
+
+#define BACKLOG 5
+
+#endif //THINKING_IN_CPP_COMMON_H
+
+
+
---
+++ network/tcp/helloworld/helloworld1.h
@@ -0,0 +1,13 @@
+//
+// Created by fufeng on 2023/12/6.
+//
+
+#ifndef THINKING_IN_CPP_HELLOWORLD1_H
+#define THINKING_IN_CPP_HELLOWORLD1_H
+
+class helloworld1 {
+
+};
+
+
+#endif //THINKING_IN_CPP_HELLOWORLD1_H
