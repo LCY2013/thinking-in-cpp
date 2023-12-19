0ca8ed34c25042b838ef77fdfd1ec2709729af26
fufeng <15281205719@163.com>
2023-12-19 21:42:24 +0800

    Feat: C++ ptr

--- basic/ptr/ptr_intro.cpp
+++ basic/ptr/ptr_intro.cpp
@@ -3,17 +3,56 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-int main() {
-  int a = 7, b = -1;
-  float c = 3.14f;
-  int *d = &a;
-  float *e = &c;
-  cout << d << endl;
-  cout << e << endl;
-  cout << *d << endl;
-  cout << *e << endl;
+void demo01() {
+    cout << __FUNCTION__ << endl;
+    int a = 7, b = -1;
+    float c = 3.14f;
+    int *d = &a;
+    float *e = &c;
+    cout << d << endl;
+    cout << e << endl;
+    cout << *d << endl;
+    cout << *e << endl;
+}
 
+void demo02() {
+    cout << __FUNCTION__ << endl;
+    //T*: 注意*在定义和间接访问上的作用
+    int i = 3;
+    int *ip = &i;
+    cout << *ip << endl;
+    double d = 3.14;
+    double *dp = &d;
+    cout << *dp << endl;
+    char c = 'a';
+    char *cp = &c;
+    cout << *cp << endl;
+}
+
+void demo03() {
+    cout << __FUNCTION__ << endl;
+    // array of pointers 和 a pointer to an array
+    int c[4] = {0x8000000, 0xFFFFFFF, 0x000000000, 0x7FFFFFFF};
+    int *a[4]; // array of pointers 指针的数组
+    int(*b)[4]; // pointer to an array 指向数组的指针
+    b = &c; // 注意这里数组的个数得要匹配
+    // 将数组中元素赋给数组a
+    for (int i = 0; i < 4; ++i) {
+        a[i] = &c[i];
+    }
+    // 输出结果
+    cout << *(a[0]) << endl;
+    cout << (*b)[3] << endl;
+}
+
+int main() {
+    demo01();
+
+    demo02();
+
+    demo03();
     return 0;
 }
