f80c5a9eecb5d8baad90c1c7ce351161f5ebc2e1
扶风 <fufeng@staruniongame.com>
2024-02-06 18:53:44 +0800

    Feat: c++ module

---
+++ cpp20/ModuleGettingStarted/src/helloworld.cpp
@@ -0,0 +1,21 @@
+//
+// Created by fufeng on 2024/2/6.
+//
+export module helloworld;
+
+import <iostream>;
+import <cstdint>;
+
+void private_hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+export void hello() {
+    std::cout << "Hello world!" << std::endl;
+}
+
+namespace hname {
+    export int32_t getNumber() {
+        return 10;
+    }
+}
---
+++ cpp20/ModuleGettingStarted/src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by fufeng on 2024/2/6.
+//
+#include <iostream>
+
+import helloworld;
+
+int main() {
+    hello();
+    std::cout << "Hello " << hname::getNumber() << std::endl;
+
+    return 0;
+}
